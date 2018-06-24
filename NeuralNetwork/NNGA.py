d6cb59090c995b8e3da45a6a920d7c1109e995f1
Adam Power <31990347+adampower48@users.noreply.github.com>
2018-06-24 15:09:01 +0100

    Added fully random breeding.

--- NeuralNetwork/NNGA.py
+++ NeuralNetwork/NNGA.py
@@ -8,8 +8,8 @@ from NeuralNetwork.Network import Network
 
 
 class NNGA(GeneticAlgorithm):
-    def __init__(self, dimensions, pop_size=50, generations=100, mutate_rate=0.05, breed_rate=0.9, max_time=0,
-                 verbose_interval=1, **kwargs):
+    def __init__(self, dimensions, pop_size=50, generations=20, mutate_rate=0.06, breed_rate=0.9, max_time=0,
+                 verbose_interval=10, **kwargs):
         super().__init__(pop_size=pop_size, generations=generations, mutate_rate=mutate_rate, breed_rate=breed_rate,
                          max_time=max_time, verbose_interval=verbose_interval, **kwargs)
 
@@ -32,12 +32,31 @@ class NNGA(GeneticAlgorithm):
             "score": 0
         }
 
-        output = simulate(genome, self.dimensions)
-        r["score"] = - MSError(actual_outputs, output)
+        r["score"] = - simulate(genome, self.dimensions)
 
         return r
 
     def breed(self, p1, p2):
+        # Fully random bit selection
+        # rands = np.random.rand(self.genome_size)
+        # r = 0
+        # child = []
+        # for i in range(len(p1)):
+        #     arr_i = []
+        #     for j in range(len(p1[i])):
+        #         arr_j = []
+        #         for k in range(len(p1[i][j])):
+        #             if rands[r] < 0.5:
+        #                 arr_j.append(p1[i][j][k])
+        #             else:
+        #                 arr_j.append(p2[i][j][k])
+        #             r += 1
+        #
+        #         arr_i.append(np.array(arr_j))
+        #     child.append(np.array(arr_i))
+        # child = np.array(child)
+
+        # One-points crossover
         xpos = random.randrange(len(p1))
         if random.random() < 0.5:
             child = np.concatenate((p1[:xpos], p2[xpos:]))
@@ -45,7 +64,7 @@ class NNGA(GeneticAlgorithm):
             child = np.concatenate((p2[:xpos], p1[xpos:]))
         child = deepcopy(child)
 
-        # Randomly choose bits to mutate todo: fix this, Needs to work with ndarrays.
+        # Randomly choose bits to mutate
         n_mutates = np.random.poisson(self.MUTATE_RATE * self.genome_size)
         for _ in range(n_mutates):
             i = random.randrange(len(child))
@@ -59,18 +78,31 @@ class NNGA(GeneticAlgorithm):
 
 
 def simulate(genome, dims):
+    tot_error = 0
     net = Network(genome, dims)
-    return net.feedforward(actual_inputs)
+    for i in range(len(actual_inputs)):
+        outputs = net.feedforward(actual_inputs[i])
+        error = MSError(actual_outputs[i], outputs)
+        tot_error += error
+
+    return tot_error
 
 
 def MSError(actual, predicted):
     return sum(map(lambda a, p: (a - p) ** 2, actual, predicted))
 
 
-actual_inputs = np.array([1, 0, 0])
-actual_outputs = np.array([1, 0, 1])
+# actual_inputs = np.array([1, 0, 0])
+# actual_outputs = np.array([1, 0, 1])
+actual_inputs = np.array([[0, 0],
+                          [0, 1],
+                          [1, 0],
+                          [1, 1]])
+actual_outputs = np.array([[0], [1], [1], [0]])
 
 if __name__ == '__main__':
-    nnga = NNGA([3, 4, 4, 3])
+    nnga = NNGA([2, 4, 1])
     best_weights = nnga.run()
     print(*best_weights.tolist(), sep="\n\n")
+    net = Network(best_weights, [2, 4, 1])
+    print(net.feedforward(actual_inputs[3]))
